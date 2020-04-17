9dc4a798a3ebf568585567ea50c463d2f01d6cb3
SaakshiShah25 <shahsaakshi25@gmail.com>
2020-04-17 20:01:41 +0530

    Signup successful

--- trana/tranaApp/urls.py
+++ trana/tranaApp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 path('',views.home,name='home'),
+path('signup/',views.signup,name='signup'),
 path('login/',views.login_view,name='login'),
 path('postlogin/',views.post_login,name='postlogin'),
 path('postlogin/logout/',views.logout_view,name='logout'),
--- trana/tranaApp/views.py
+++ trana/tranaApp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.contrib import auth
+from firebase import firebase
 # Create your views here.
-import pyrebase
+#import pyrebase
+import firebase_admin
+from firebase_admin import credentials,firestore,auth
+cred=credentials.Certificate('./ServiceAccountKey.json')
+default_app=firebase_admin.initialize_app(cred)
+db=firestore.client()
 
 config = {
   'apiKey': "AIzaSyDXMensLWaA5AvH2M7r6DhR6oCIu1kXG5U",
@@ -12,14 +18,31 @@ config = {
   'storageBucket': "trana-cfbcf.appspot.com",
   'messagingSenderId': "105597355531",
   'appId': "1:105597355531:web:b7d7645d90867fb34a690e",
-  'measurementId': "G-FQ9WQ7Z7VQ"
+  'measurementId': "G-FQ9WQ7Z7VQ",
+ 
 }
 
-firebase = pyrebase.initialize_app(config)
-authe=firebase.auth()
+#firebase = pyrebase.initialize_app(config)
+#authe=firebase.auth()
+#db = firebase.database()
+#firebase = firebase.FirebaseApplication("https://trana-cfbcf.firebaseio.com", None)
 
 def home(request):
     return render(request,'home.html')
+
+def signup(request):
+    name=request.POST.get('username')
+    email=request.POST.get('email')
+    position=request.POST.get('position')
+    password=request.POST.get('password1')
+
+    firebase_admin.auth.create_user(email=email,password=password)
+    data={'name':name,'position':position}
+    db.collection(u'authorities').document(u'details').set(data)
+    
+   
+   
+    return render(request,'signup.html')
 
 def login_view(request):
     return render(request,'login.html')
