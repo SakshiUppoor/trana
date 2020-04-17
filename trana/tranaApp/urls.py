51aab1582586e8fbae208dc36acd1eeef730f574
SaakshiShah25 <shahsaakshi25@gmail.com>
2020-04-17 12:41:43 +0530

    Logout successful

--- trana/tranaApp/urls.py
+++ trana/tranaApp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 path('',views.home,name='home'),
 path('login/',views.login_view,name='login'),
-#path('logout/',views.logout_view,name='logout'),
 path('postlogin/',views.post_login,name='postlogin'),
+path('postlogin/logout/',views.logout_view,name='logout'),
 ]
--- trana/tranaApp/views.py
+++ trana/tranaApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from django.contrib import auth
 # Create your views here.
 import pyrebase
 
@@ -15,7 +16,7 @@ config = {
 }
 
 firebase = pyrebase.initialize_app(config)
-auth=firebase.auth()
+authe=firebase.auth()
 
 def home(request):
     return render(request,'home.html')
@@ -27,8 +28,15 @@ def post_login(request):
     email=request.POST['email']
     password=request.POST['password']
     try:
-        user = auth.sign_in_with_email_and_password(email, password)
+        user = authe.sign_in_with_email_and_password(email, password)
     except:
-        messages.error("Invalid Credentials")
+        messages.error(request,"Invalid Credentials")
         return render(request,'login.html')
+    print(user)
+    session_id=user['idToken']
+    request.session['uid']=str(session_id)
     return render(request,'postlogin.html',{'e':email})
+
+def logout_view(request):
+    auth.logout(request)
+    return render(request,'login.html')
