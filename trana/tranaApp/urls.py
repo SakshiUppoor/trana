2b08354d33ab3ecf1e7a3de947344f83e7289780
SaakshiShah25 <shahsaakshi25@gmail.com>
2020-04-18 10:19:07 +0530

    merged

--- trana/tranaApp/urls.py
+++ trana/tranaApp/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('',views.home,name='home'),
-path('signup/',views.signup,name='signup'),
-path('login/',views.login_view,name='login'),
-path('postlogin/',views.post_login,name='postlogin'),
-path('postlogin/logout/',views.logout_view,name='logout'),
+    path("", views.home, name="home"),
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.login_view, name="login"),
+    path("postlogin/", views.post_login, name="postlogin"),
+    path("postlogin/logout/", views.logout_view, name="logout"),
+    path("reports/", views.reportsDashboard, name="reports"),
 ]
