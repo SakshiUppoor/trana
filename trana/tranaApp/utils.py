c561b0279f51831007c65b251b8588dc7cfd406e
Sanket Shah <53148165+sanket1703@users.noreply.github.com>
2020-04-26 15:21:56 +0530

    Distance Matrix added in utils

--- trana/tranaApp/utils.py
+++ trana/tranaApp/utils.py
@@ -1,4 +1,5 @@
 import smtplib
+import math
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from .email_credentials import email, pwd
@@ -26,3 +27,22 @@ def send_mail(email_id, details, medicine):
 
     s.sendmail(me, you, msg.as_string())
     s.quit()
+    
+  def isInRadius(lat1,long1,lat2,long2):
+    R = 6373.0
+    lat1 = math.radians(lat1)
+    long1 = math.radians(long1)
+    lat2 = math.radians(lat2)
+    long2 = math.radians(long2)
+
+    dlon = long2 - long1
+    dlat = lat2 - lat1
+
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+    if distance<5000:
+        return true
+    else:
+        return false
+  
