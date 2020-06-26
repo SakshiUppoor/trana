91bb681de8572a34fa68c51aafe7f8859e845e5a
SaakshiShah25 <shahsaakshi25@gmail.com>
2020-06-26 20:22:29 +0530

    Added team info

--- tranaApp/utils.py
+++ tranaApp/utils.py
@@ -33,7 +33,6 @@ def send_mail(to, subject, message):
     yag = yagmail.SMTP('medtrana2020',pwd)
     yag.send(to = to, subject = subject, contents = message)
 
-
 def medicine_available(email_id, details, medicine):
     subject = "Request for medicine {}".format(medicine)
     message = "<html><body>The medicine {} you requested is available at the pharmacy {} located in {}.</body></html>".format(
@@ -50,7 +49,7 @@ def send_verification_mail(request, details):
         "medtrana2020@gmail.com",
         "shahsaakshi25@gmail.com",
         "sanketyou8@gmail.com",
-        #"",
+        "siddhi2000jhun@gmail.com",
         #"",
     ]
     print("~~~~~~~~~~~~",type(details["uId"]), type('True'))
@@ -63,25 +62,48 @@ def send_verification_mail(request, details):
 
     send_mail(super_admin_email_list, subject, message)
 
-def send_result(email_id, user, accepted):
+def dr_send_verification_mail(request, details):
+    current_site = get_current_site(request)
+
+    super_admin_email_list = [
+        "sakshiuppoor@gmail.com",
+        "phani.lav@gmail.com",
+        "medtrana2020@gmail.com",
+        "shahsaakshi25@gmail.com",
+        "sanketyou8@gmail.com",
+        "siddhi2000jhun@gmail.com",
+        #"",
+    ]
+    print("~~~~~~~~~~~~",type(details["uId"]), type('True'))
+    subject = "New Doctor Sign-Up"
+    message = render_to_string("dr_verification_mail.html",
+    {
+        "domain":current_site.domain,
+        "details":details,
+    })
+
+    send_mail(super_admin_email_list, subject, message)
+
+def send_result(email_id, user, accepted, position):
     print(user)
     #print(user.to_dict())
     print(user.__dict__)
     if accepted == 'True':
-        subject = "Authority Account Application Verified"
-        message = """<html><body>You can now log in to your authority dashboard.
+        subject = position + " Account Application Verified"
+        message = """<html><body>You can now log in to your {} account.
         <br>
         <b>Email ID:</b> {}
         <br>
-        <html><body>""".format(email_id)
+        <html><body>""".format(position,email_id)
         print(message)
         send_mail(email_id, subject, message)
 
     else:
-        subject = "Authority Account Application Rejected"
-        message = """<html><body>Your application for the position of authority at Trana has been rejected.
-        <html><body>"""
+        subject = position + " Account Application Rejected"
+        message = """<html><body>Your application for the position of {} at Trana has been rejected.
+        <html><body>""".format(position)
         send_mail(email_id, subject, message)
+
 
 def send_contact_mail(request, details):
     current_site = get_current_site(request)
@@ -92,7 +114,7 @@ def send_contact_mail(request, details):
         "medtrana2020@gmail.com",
         "shahsaakshi25@gmail.com",
         "sanketyou8@gmail.com",
-        #"",
+        "siddhi2000jhun@gmail.com",
         #"",
     ]
    
@@ -104,3 +126,21 @@ def send_contact_mail(request, details):
     })
 
     send_mail(super_admin_email_list, subject, message)
+
+
+def isInRadius(lat,lon,lat2,lon2):
+    R = 6373.0
+    lat1 = math.radians(lat)
+    long1 = math.radians(lon)
+    lat2 = math.radians(lat2)
+    long2 = math.radians(lon2)
+    dlon = long2 - long1
+    dlat = lat2 - lat1
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+    print(distance)
+    if distance<10:
+        return True
+    else:
+        return False
